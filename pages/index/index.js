c8742f81c5bfcda680c3cd8873c99ecd257e9e53
赖铭志 <ramen.curator@gmail.com>
2021-12-24 00:31:55 +0800

    complete to show question: when loading, scroll-dowm, fault happen

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,1 +1,35 @@
-Page({})
+const data = new Array(10).fill(null).map((v, i, a) => (a[i] = i));
+
+const fakeAPI = () => {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res(data);
+    }, 1000);
+  });
+};
+
+Page({
+  data: {
+    dataList: [],
+    enabled: false,
+    isFirstRender: true,
+    triggered: true,
+  },
+  onLoad() {
+    fakeAPI().then((res) =>
+      this.setData({ dataList: res }, () => {
+        this.setData({ enabled: true });
+      })
+    );
+  },
+  onPageScroll(e) {
+    this.setData({ enabled: e.scrollTop <= 0 });
+  },
+  onRefresherRefresh() {
+    this.setData({ triggered: true }, () => {
+      fakeAPI().finally(() => {
+        this.setData({ triggered: false });
+      });
+    });
+  },
+});
